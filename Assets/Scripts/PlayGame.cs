c7bde9462fd03a323974cff3faf29fc008ceedd1
David Mendaño <david.mendano@movetia.com>
2019-01-06 17:23:07 +0100

    final screen

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour
+{
+
+    private const string SCREEN_TO_OPEN = "PlayScreen";
+
+    public void LaunchPlayScreen()
+    {
+        SceneManager.LoadScene(SCREEN_TO_OPEN);
+    }
+}
